bc10935e739c2dc32f29c34e4cf829f59f486279
sanketkhandekar86 <sanket.khandekar@northgateps.com>
2021-05-23 15:30:44 +0100

    Completed UI elements for Team page

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,35 +1,31 @@
-import  {React, useEffect,useState } from 'react';
-import { MatchDetailCard } from '../components/MatchDetailCard';
-import { MatchSmallCard } from '../components/MatchSmallCard';
+import { React, useEffect, useState } from "react";
+import { MatchDetailCard } from "../components/MatchDetailCard";
+import { MatchSmallCard } from "../components/MatchSmallCard";
+import { useParams } from "react-router-dom";
 
+export const TeamPage = () => {
+  const [team, setTeam] = useState({ matches: [] });
+  const { teamName } = useParams();
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const response = await fetch(`http://localhost:8080/team/${teamName}`);
+      const data = await response.json();
 
-export const TeamPage = () =>{
-
-  const  [team,setTeam] = useState({matches:[] });
-
-  useEffect(
-    () => {
-        const fetchMatches = async () =>{
-           const response = await fetch('http://localhost:8080/team/Mumbai Indians') 
-              const data = await response.json();
-              
-              setTeam(data);
-     
-          };
-        fetchMatches();
-    },[]
-
-  );
-  return (
-      <div className="TeamPage">
-        <h1>{team.teamName}</h1>
-
-
-        <MatchDetailCard match={team.matches[0]}/>
-        {team.matches.slice(1).map(match => <MatchSmallCard match ={match}/>)}
-      
-        
-      </div>
-    );
+      setTeam(data);
+    };
+    fetchMatches();
+  }, [teamName]);
+  if (!team || !team.teamName) {
+    return <h1> Team not found</h1>;
   }
-  
+  return (
+    <div className="TeamPage">
+      <h1>{team.teamName}</h1>
+
+      <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      {team.matches.slice(1).map((match) => (
+        <MatchSmallCard teamName={team.teamName} match={match} />
+      ))}
+    </div>
+  );
+};
