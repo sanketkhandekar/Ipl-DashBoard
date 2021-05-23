d8f12cf2b643a941921057fda5e52fc98eec4ff8
sanketkhandekar86 <sanket.khandekar@northgateps.com>
2021-05-23 16:25:49 +0100

    Match Page Checkpoint

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,15 +1,33 @@
 import { React, useEffect, useState } from "react";
-
+import { MatchDetailCard } from "../components/MatchDetailCard";
+import { MatchSmallCard } from "../components/MatchSmallCard";
 import { useParams } from "react-router-dom";
 
 export const MatchPage = () => {
- 
+  const [matches, setMatches] = useState([]);
+  const { teamName,year } = useParams();
   
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const response = await fetch(
+        `http://localhost:8080/team/${teamName}/matches?year=${year}`);
+
+        const data = await response.json();
+
+        setMatches(data);
+  
+    };
+    fetchMatches();
+
+     }, []
+  );
   return (
     <div className="MatchPage">
-      <h1>Match Page </h1>
+      <h1>Match Page </h1>  
+        {
+            matches.map(match => <MatchDetailCard teamName={teamName} match={match} />)
 
-    
+        }
     </div>
   );
-};
+}
