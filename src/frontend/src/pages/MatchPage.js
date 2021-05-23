215b8afab52916e8cad4cd7eca978c52b8c08e76
sanketkhandekar86 <sanket.khandekar@northgateps.com>
2021-05-23 22:53:40 +0100

    Stable code around CSS

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -2,7 +2,8 @@ import { React, useEffect, useState } from "react";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 import { useParams } from "react-router-dom";
-
+import './MatchPage.scss';
+import { YearSelector } from "../components/YearSelector";
 export const MatchPage = () => {
   const [matches, setMatches] = useState([]);
   const { teamName,year } = useParams();
@@ -19,15 +20,21 @@ export const MatchPage = () => {
     };
     fetchMatches();
 
-     }, []
+     }, [teamName,year]
   );
   return (
     <div className="MatchPage">
-      <h1>Match Page </h1>  
+      <div className="year-selector">
+        <h3> Select Year </h3>
+         <YearSelector teamName={teamName}/>
+      </div>
+      <div> 
+        <h1 className="page-heading">{teamName} matches in {year}</h1> 
         {
             matches.map(match => <MatchDetailCard teamName={teamName} match={match} />)
 
         }
+        </div>
     </div>
   );
 }
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -3,6 +3,7 @@ import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 import { useParams } from "react-router-dom";
 import { PieChart } from "react-minimal-pie-chart";
+import {Link} from 'react-router-dom';
 
 import "./TeamPage.scss";
 
@@ -45,7 +46,8 @@ export const TeamPage = () => {
         <MatchSmallCard teamName={team.teamName} match={match} />
       ))}
       <div className="more-link">
-        <a href="#">More ></a>
+      <Link to={`/teams/${teamName}/matches/${process.env.REACT_APP_DATA_END_YEAR}`}> More ></Link> 
+        
       </div>
     </div>
   );
